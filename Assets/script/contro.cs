47b47209fdbff49d0e96b6d6f16da2c70d40cd0a
Mingyu Chi <chimi@shernet.sheridancollege.ca>
2026-03-09 11:10:27 -0400

    save

---
+++ Assets/script/contro.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class contro : MonoBehaviour
+{
+    public float speed = 5;
+    public Vector2 movement;
+    public AudioSource a;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += (Vector3)movement * speed * Time.deltaTime;
+        transform.position = movement;
+    }
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        movement = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
+
+
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+
+        
+    }
+
+    public void OnAttak(InputAction.CallbackContext context)
+    {
+        Debug.Log("attack" + context.phase);
+        if(context.performed == true)
+        {
+            a.Play();
+        }
+    }
+}
