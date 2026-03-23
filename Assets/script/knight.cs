bc26f6cc3d1fe2215cfd1a22a4802b9d17586228
Mingyu Chi <chimi@shernet.sheridancollege.ca>
2026-03-23 11:55:35 -0400

    code gym

--- Assets/script/knight.cs
+++ Assets/script/knight.cs
@@ -1,8 +1,14 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class knight : MonoBehaviour
 {
     public AudioSource sfx;
+    public ParticleSystem particles;
+    public Vector2 m;
+    public float speed = 5;
+    public Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,11 +18,46 @@ public class knight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += (Vector3)m * speed * Time.deltaTime;
     }
 
     public void footsteps()
     {
       sfx.Play();
     }
+
+    public void play()
+    {
+        particles.Play();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        animator.SetBool("run", true);
+        m = context.ReadValue<Vector2>();
+    }
+
+    public void Onjump(InputAction.CallbackContext context)
+    {
+        StartCoroutine(jp());
+
+    }
+
+
+
+    IEnumerator jp()
+    {
+        animator.SetBool("jump", true);
+        float t = 0;
+
+        while (t < 0.7 )
+        {
+            t += Time.deltaTime;
+            yield return null;
+        }
+        animator.SetBool("jump", false);
+
+    }
+
+
 }
