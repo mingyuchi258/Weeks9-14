d3bb84aa05cc392af48e4be6272f2360f25974de
mingyuchi258 <chimi@sheridancollege.ca>
2026-04-02 15:41:06 -0400

    The generation and determination of medicine have been added, along with annotations.

--- Assets/Audio/assignments/Ghost.cs
+++ Assets/Audio/assignments/Ghost.cs
@@ -6,6 +6,7 @@ public class Ghost : MonoBehaviour
 
     void Update()
     {
+
         Vector3 SC = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, 0));
         transform.position = Vector3.MoveTowards(transform.position, SC, 0.5f * Time.deltaTime);
 
--- Assets/Audio/assignments/gmaker.cs
+++ Assets/Audio/assignments/gmaker.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class gmaker : MonoBehaviour
 {
-    public GameObject Ghost;
+    //the prefab to be generated
+    public GameObject PF;
     public movecontro player;
-    public float contactDistance = 1f;
+    //triggered when the prefab comes into contact with the stronghold
+    public UnityEvent GhostAttack;
+    //Triggered when the prefab disappears after being attacked by a player
+    public UnityEvent HealthEffect;
 
-    List <GameObject> GL;
+    //list that stores prefabs
+    List<GameObject> GL;
     public bool gameStart = true;
+    //Number of prefabs generated each time
     float x = 1;
+    //Waiting time between each generation
     float waitSpeed = 15;
 
 
     void Start()
     {
-        GL = new List <GameObject> ();
+        GL = new List<GameObject>();
+
         StartCoroutine(InstanGhostV());
+        StartCoroutine(InstanHealthV());
     }
 
     void Update()
@@ -25,17 +35,24 @@ public class gmaker : MonoBehaviour
 
         for (int i = GL.Count - 1; i >= 0; i--)
         {
-            GameObject ghost = GL[i];
+            GameObject PF = GL[i];
 
-            float distance = Vector2.Distance(ghost.transform.position, player.Weapon.position);
-            if (distance < contactDistance)
+            // Detect the distance between the prefab and the weapon
+            float distance = Vector2.Distance(PF.transform.position, player.Weapon.position);
+            //If they come into contact, an event is triggered and destroy prefab
+            if (distance < 1)
             {
-                Destroy(ghost);
+                HealthEffect.Invoke();
+                Destroy(PF);
                 GL.RemoveAt(i);
             }
-            else if (ghost.transform.position.x > -1 && ghost.transform.position.x < 1 && ghost.transform.position.y > -1 && ghost.transform.position.y < 1)
+
+            //Detect whether the prefab has contacted the stronghold
+            else if (PF.transform.position.x > -1 && PF.transform.position.x < 1 && PF.transform.position.y > -1 && PF.transform.position.y < 1)
             {
-                Destroy(ghost);
+                //If get close to the stronghold, trigger the event, and destroy the prefab
+                GhostAttack.Invoke();
+                Destroy(PF);
                 GL.RemoveAt(i);
             }
         }
@@ -43,8 +60,10 @@ public class gmaker : MonoBehaviour
 
     IEnumerator InstanGhostV()
     {
+        //Ghost generation rule
         while (gameStart == true)
         {
+            //After generating the prefab, wait 15 seconds before generating it again. Each time, generate one more prefab than the last time, and wait 3 seconds longer than the last time
             InstanGhost();
             yield return new WaitForSeconds(waitSpeed);
             x++;
@@ -52,14 +71,32 @@ public class gmaker : MonoBehaviour
         }
     }
 
-    
     void InstanGhost()
     {
+        //Randomly generate a specified number of prefabs within the specified range and add them to the list
         for (int i = 0; i < x; i++)
         {
             Vector3 randomP = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), 0);
-            GameObject newGhost = Instantiate(Ghost, transform.position + randomP, Quaternion.identity);
+            GameObject newGhost = Instantiate(PF, transform.position + randomP, Quaternion.identity);
             GL.Add(newGhost);
         }
     }
+
+    IEnumerator InstanHealthV()
+    {
+        //Medicine generation rule
+        while (gameStart == false)
+        {
+            //Randomly generate a specified number of prefabs within the specified range and add them to the list
+            for (int i = 0; i < x; i++)
+            {
+                Vector3 randomP = new Vector3(Random.Range(-10, 10), Random.Range(-5, 5), 0);
+                GameObject newGhost = Instantiate(PF, transform.position + randomP, Quaternion.identity);
+                GL.Add(newGhost);
+            }
+            //Generates every 40 seconds
+            yield return new WaitForSeconds(40);
+        }
+    }
+
 }
