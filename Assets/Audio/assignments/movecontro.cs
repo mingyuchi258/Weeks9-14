a2f1e9f5df034293120b143297fa00b3778eb429
mingyuchi258 <chimi@sheridancollege.ca>
2026-04-02 16:11:08 -0400

    Add comments

--- Assets/Audio/assignments/movecontro.cs
+++ Assets/Audio/assignments/movecontro.cs
@@ -20,37 +20,40 @@ public class movecontro : MonoBehaviour
     {
         transform.position += (Vector3)movement * speed * Time.deltaTime;
     }
-
+    //Move value
     public void OnMove(InputAction.CallbackContext context)
     {
+
         movement = context.ReadValue<Vector2>();
     }
-
+    //Attack value
     public void OnAttack(InputAction.CallbackContext context)
     {
-        Debug.Log("attack");
+        //Stop if weapons are being used
         if (attackC != null)
         {
             StopCoroutine(attackC);
         }
+        //Start use weapon attack effect
         attackC = StartCoroutine(useWeapon());
     }
 
+    //Use weapon attack effect
     IEnumerator useWeapon()
     {
         float R = 0;
-
         while (R < 360)
         {
-
+            //The weapon rotates once during the attack
             Vector3 newR = weapon.eulerAngles;
             newR.z -= 700 * Time.deltaTime;
             weapon.eulerAngles = newR;
             R += 700 * Time.deltaTime;
             yield return null;
         }
+        //After rotating, the weapon will return to its original position
         weapon.eulerAngles = Vector3.zero;
-
+        //Do not use attack effects after the coroutine end
         attackC = null;
     }
 }
